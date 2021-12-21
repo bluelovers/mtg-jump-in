b1106728927bda3391d59d4f896210c3cb81c5e3
bluelovers <codelovers@users.sourceforge.net>
2021-12-21 21:05:02 +0800

    ci: fix Cannot find module '../__root'

---
+++ test/__root.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * Created by user on 2021/12/5.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.__root = void 0;
+const path_1 = require("path");
+exports.__root = (0, path_1.join)(__dirname, '..');
+//# sourceMappingURL=__root.js.map
